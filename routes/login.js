2d4888d02f247976833aa2133d570601c018e91b
Sarah Avery <70301632+SarahAvery@users.noreply.github.com>
2021-07-23 19:47:47 -0400

    create queries test folder (#25)

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 
 module.exports = () => {
   router.get("/", (req, res) => {
