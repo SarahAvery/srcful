82ff03a068f93d775dcce58945c8e0c1dad3df95
Sarah Avery <70301632+SarahAvery@users.noreply.github.com>
2021-07-23 23:13:07 -0400

    Base schemas and seeds (#27)
    * base schema and seeds
    
    * add more data to seed files, to better demonstrate sql queries

--- db/seeds/05_resource_likes.sql
+++ db/seeds/05_resource_likes.sql
@@ -3,12 +3,43 @@ resource_likes (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
-);
+),
 */
 
-
+-- 1 - 33 user
+-- 1 - 33 res
 
 INSERT INTO resource_likes (user_id, resource_id) 
 VALUES (1, 1),
 (2, 1),
-(2, 2);
+(2, 2),
+(5, 7),
+(6, 21),
+(11, 21),
+(1, 11),
+(26, 28),
+(30, 30),
+(20, 5),
+(27, 33),
+(23, 21),
+(28, 5),
+(3, 3),
+(22, 21),
+(7, 16),
+(15, 22),
+(16, 14),
+(13, 23),
+(31, 1),
+(12, 17),
+(21, 30),
+(28, 29),
+(8, 33),
+(13, 18),
+(5, 11),
+(31, 6),
+(1, 9),
+(20, 1),
+(21, 2),
+(25, 33),
+(33, 1),
+(6, 14);
