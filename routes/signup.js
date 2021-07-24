329edaade9703f89c901b9c4d86be1360a83d967
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-24 22:57:15 +0000

    Create initial /signup POST route

--- routes/signup.js
+++ routes/signup.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
+const bcrypt = require('bcrypt');
 
 // app.set("view engine", "ejs");
-
-// app.get('/signup', (req, res) => {
-//   res.render('signup');
-// });
 
 module.exports = () => {
   router.get("/", (req, res) => {
     res.render("signup");
+  });
+
+  router.post("/", (req, res) => {
+    const { username, email, password } = req.body;
+
+    // Hash password
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    //Store new user in database: login query
+    // userDatabase[id] = { id, email, hashedPassword };
+
+    //Create cookie session
+    //Select userID from db via username?
+    // req.session.userID = id;
+
+    // Redirect to /myresources
+    res.redirect('/my-resources');
   });
   return router;
 };
