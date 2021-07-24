0a9a1ac1faf41a763c831ecca48a07fec1aaf886
Sarah Avery <70301632+SarahAvery@users.noreply.github.com>
2021-07-24 18:45:52 -0400

    Profile template (#43)
    * start of profile page
    
    * Edit button opens edit panel using jquery.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,61 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
+
+(function ($) {
+  $(document).ready(function () {
+    // Profile Edit Btn
+    $(".profile-container")
+      .find(".edit-btn")
+      .on("click", function () {
+        if ($(".edit-btn").hasClass("active")) {
+          $(".edit-btn").removeClass("active");
+        } else {
+          $(".edit-btn").addClass("active");
+        }
+      });
+
+    // Popout Edit
+    $(".profile-container")
+      .find(".edit-btn")
+      .on("click", function () {
+        if ($(".edit-btn").hasClass("active")) {
+          $(".edit-popout").removeClass("hidden");
+        } else {
+          $(".edit-popout").addClass("hidden");
+        }
+      });
+
+    // DONT REMOVE BELOW CODE PLEASE
+
+    //   const animateTopHandler = function () {
+    //     $("html").animate({ scrollTop: 0 }, "slow");
+    //   };
+
+    //   $("#topBtn").on("click", animateTopHandler);
+    // });
+
+    // $(document).scroll(function () {
+    //   // scroll button
+    //   const scrollPosition = $(window).scrollTop();
+    //   if (scrollPosition > 100) {
+    //     $("#topBtn").css("visibility", "visible");
+    //   } else {
+    //     $("#topBtn").css("visibility", "hidden");
+    //   }
+
+    //   // nav
+    //   if (scrollPosition > 300) {
+    //     $("body").addClass("scrolled");
+    //   } else {
+    //     $("body").removeClass("scrolled");
+    //   }
+  });
+})(jQuery);
