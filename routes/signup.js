3054200d53d1d21ce0628fb3e42215493a925d13
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-25 16:27:19 +0000

    Store new user in db

--- routes/signup.js
+++ routes/signup.js
@@ -5,23 +5,40 @@ const bcrypt = require('bcrypt');
 
 // app.set("view engine", "ejs");
 
-module.exports = () => {
+module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("signup");
   });
 
   router.post("/", (req, res) => {
-    const { username, email, password } = req.body;
+    const user = req.body;
 
     // Hash password
-    const hashedPassword = bcrypt.hashSync(password, 10);
+    user.password = bcrypt.hashSync(user.password, 12);
 
-    //Store new user in database: login query
-    // userDatabase[id] = { id, email, hashedPassword };
+    //If username taken, error
+    //If email already registered, error
+    //If inputs blank, error (currently required in html)
 
-    //Create cookie session
-    //Select userID from db via username?
-    // req.session.userID = id;
+    // Else store new user in database: login query
+    db.query(`
+      INSERT INTO users(username, email, password)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+      `, [user.username, user.email, user.password])
+      .then(data => {
+        const newUser = data.rows[0];
+        if (!newUser) {
+          res.send({error: "error"});
+        } else {
+          req.session.userId = user.id;
+          res.send("🤗");
+        }
+      })
+      .catch(e => {
+        res.status(500);
+        res.send(e);
+      })
 
     // Redirect to /myresources
     res.redirect('/my-resources');
