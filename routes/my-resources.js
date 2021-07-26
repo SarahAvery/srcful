2c4d586a843f92d24aeb46cc5cf49db3ee448efe
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-26 15:53:44 +0000

    Add POST /my-resources/new route

--- routes/my-resources.js
+++ routes/my-resources.js
@@ -2,13 +2,7 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 
-// app.set("view engine", "ejs");
-
-// app.get('/signup', (req, res) => {
-//   res.render('signup');
-// });
-
-module.exports = () => {
+module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("my-resources");
   });
@@ -16,5 +10,32 @@ module.exports = () => {
   router.get("/new", (req, res) => {
     res.render("resources_new");
   });
+
+  router.post("/new", (req, res) => {
+    const resource = req.body;
+    //All fields mandatory
+    //If user logged in, store new resource in db, store cateogry in resource_categories
+    const queryString = `
+      INSERT INTO resources (title, description, url, image_url, creator_id)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;
+    `;
+    db.query(queryString, [resource.title, resource.description, resource.url, resource.imageURL, req.session.userId]
+    )
+      .then((data) => {
+        const newResource = data.rows[0];
+        if (!newResource) {
+          res.send({ error: "error" });
+        } else {
+          res.redirect("/my-resources");
+        }
+      })
+      .catch((e) => {
+        res.status(500);
+        res.send(e.stack);
+      });
+
+  });
+
   return router;
 };
