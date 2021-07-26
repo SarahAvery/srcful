8a2da4d56f7a7ab0e9f5658c926035f2ce4bb595
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-26 04:35:26 +0000

    Add POST login route

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const router = express.Router();
 // const fetch = require("node-fetch");
+const bcrypt = require("bcrypt");
 
-module.exports = () => {
+module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("login");
   });
 
+  router.post("/", (req, res) => {
+    const { email, password } = req.body;
+
+    db.query(`SELECT * FROM users WHERE email = $1;`, [email])
+    .then(data => {
+      const user = data.rows[0];
+      if (bcrypt.compareSync(password, user.password)) {
+        //create cookie session: req.session.userId = user.id;
+        res.redirect("/my-resources");
+      } else {
+        //invalid password
+        res.send({error: "invalid password"});
+      }
+    })
+    .catch(e => {
+      //invalid email
+      res.status(500);
+      res.send({error: "invalid email"});
+    });
+
+  });
   return router;
 };
