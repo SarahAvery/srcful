5d4dc0da2fb7078f5990361be4bc7d5d3e412fb0
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-27 19:57:00 +0000

    Add POST /profile route

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
+const bcrypt = require("bcrypt");
 
 // THIS IS AN HTML REQUEST/RENDER
 // ALL REQUESTS SHOULD BE MOCKED USING THIS STRUCTURE
-
-
-
 module.exports = (db) => {
   router.get("/", (req, res) => {
     fetch(process.env.API_URL + "/users")
@@ -22,7 +20,7 @@ module.exports = (db) => {
               db.query(`SELECT COUNT(*) FROM resources WHERE creator_id =$1`, [req.session.userId])
                 .then ((data) => {
                   const created_resources = data.rows[0];
-                  const templateVars = { user: currentUser[0], user_likes: user_likes.count, created_resources: created_resources.count };  
+                  const templateVars = { user: currentUser[0], user_likes: user_likes.count, created_resources: created_resources.count };
                   res.render("profile", templateVars);
                 });
             });
@@ -31,6 +29,47 @@ module.exports = (db) => {
         }
     });
   });
-  
+
+  router.post("/", (req, res) => {
+    const update = req.body;
+    const templateVars = {};
+
+    //If inputs blank?
+    // Hash password
+    update.password = bcrypt.hashSync(update.password, 12);
+
+    //If username taken, error
+    // Else update user in database
+    db.query(
+      `
+      UPDATE users
+      SET username = $1,
+          password = $2
+      WHERE id = $3;
+      `,
+      [update.username, update.password, req.session.userId]
+    )
+    .then(() => {
+      res.redirect("/profile");
+    })
+    .catch((e) => {
+      res.status(500);
+      if (e.constraint === 'users_username_key') {
+        fetch(process.env.API_URL + "/users")
+        .then((data) => data.json())
+        .then((json) => {
+          if (json.users) {
+            const templateVars = { users: json.users, error: "Username already taken" };
+            res.render("profile", templateVars);
+          } else {
+            res.redirect("/");
+          }
+        });
+      } else {
+        res.send(e.stack);
+      }
+    });
+  });
+
   return router;
 };
