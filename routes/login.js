baef4e01c6a879eb744eac0b3f6d15dffb5ce084
Cameron <cameron_uniac@hotmail.com>
2021-07-27 14:50:46 -0400

    Remove duplicate code

--- routes/login.js
+++ routes/login.js
@@ -12,25 +12,6 @@ module.exports = (db) => {
     const { email, password } = req.body;
 
     db.query(`SELECT * FROM users WHERE email = $1;`, [email])
-    .then(data => {
-      const user = data.rows[0];
-      console.log(user);
-      // if (bcrypt.compareSync(password, user.password)) {
-      if (password === user.password) {
-        req.session.userId = user.id;
-        res.redirect("/my-resources");
-      } else {
-        //invalid password
-        res.send({error: "invalid password"});
-      }
-    })
-    .catch(e => {
-      // invalid email
-      res.status(500);
-      res.send({error: e});
-    });
-
-
       .then((data) => {
         const user = data.rows[0];
         // if (bcrypt.compareSync(password, user.password)) {
