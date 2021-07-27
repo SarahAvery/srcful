a812a1f7ae2f6ebc84433d6dcbb965d732139621
Cameron <cameron_uniac@hotmail.com>
2021-07-27 01:23:54 -0400

    Create edit_profile js and ejs files, write logic in profile js and ejs files, minor edits to server and login files

--- routes/login.js
+++ routes/login.js
@@ -14,7 +14,9 @@ module.exports = (db) => {
     db.query(`SELECT * FROM users WHERE email = $1;`, [email])
     .then(data => {
       const user = data.rows[0];
-      if (bcrypt.compareSync(password, user.password)) {
+      console.log(user);
+      // if (bcrypt.compareSync(password, user.password)) {
+      if (password === user.password) {
         req.session.userId = user.id;
         res.redirect("/my-resources");
       } else {
@@ -23,9 +25,9 @@ module.exports = (db) => {
       }
     })
     .catch(e => {
-      //invalid email
+      // invalid email
       res.status(500);
-      res.send({error: "invalid email"});
+      res.send({error: e});
     });
 
   });
--- routes/profile.js
+++ routes/profile.js
@@ -5,36 +5,32 @@ const fetch = require("node-fetch");
 // THIS IS AN HTML REQUEST/RENDER
 // ALL REQUESTS SHOULD BE MOCKED USING THIS STRUCTURE
 
-module.exports = () => {
+
+
+module.exports = (db) => {
   router.get("/", (req, res) => {
     fetch(process.env.API_URL + "/users")
       .then((data) => data.json())
       .then((json) => {
         if (json.users) {
-          const templateVars = { users: json.users };
-          res.render("profile", templateVars);
+          const currentUser = (json.users).filter(obj => {
+            return obj.id === req.session.userId;
+          });
+          db.query(`SELECT COUNT(*) FROM resource_likes WHERE user_id = $1`, [req.session.userId])
+            .then ((data) => {
+              const user_likes = data.rows[0];
+              db.query(`SELECT COUNT(*) FROM resources WHERE creator_id =$1`, [req.session.userId])
+                .then ((data) => {
+                  const created_resources = data.rows[0];
+                  const templateVars = { user: currentUser[0], user_likes: user_likes.count, created_resources: created_resources.count };  
+                  res.render("profile", templateVars);
+                });
+            });
         } else {
           res.redirect("/");
         }
-      });
+    });
   });
-
+  
   return router;
 };
-
-// module.exports = () => {
-//   router.get("/:id", (req, res) => {
-//     fetch(process.env.API_URL + "/users")
-//       .then((data) => data.json())
-//       .then((json) => {
-//         if (json.users) {
-//           const templateVars = { users: json.users };
-//           res.render("profile", templateVars);
-//         } else {
-//           res.redirect("/");
-//         }
-//       });
-//   });
-
-//   return router;
-// };
