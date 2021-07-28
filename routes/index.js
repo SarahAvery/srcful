a1e216a5dfefedbb4c67d9fa51f99fda2d7c687d
Kathy Tam <44310274+kathy-tam@users.noreply.github.com>
2021-07-28 16:12:45 -0500

    Feature/authentication (#110)
    * Implement authentication middleware
    
    * Toggle nav bar if user logged in or not

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,10 @@ module.exports = () => {
       .then((data) => data.json())
       .then((json) => {
         if (json.resources) {
-          const templateVars = { resources: json.resources, pageNum: 1 };
+          const templateVars = { resources: json.resources, pageNum:1 };
+          if (req.session.userId) {
+            res.redirect("/resources");
+          }
           res.render("index", templateVars);
         } else {
           res.redirect("/");
