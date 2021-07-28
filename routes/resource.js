fe4dfc6d732e36bec7378a78dff097cead0adea2
Cameron <cameron_uniac@hotmail.com>
2021-07-28 12:45:15 -0400

    Revert to prior code

--- routes/resource.js
+++ routes/resource.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    console.log(req.session.userId);
     fetch(process.env.API_URL + "/resources")
       .then((data) => data.json())
       .then((json) => {
@@ -12,19 +12,24 @@ module.exports = (db) => {
           const currentResource = json.resources.filter((obj) => {
             return obj.resource_id === Number(req.baseUrl.slice(10));
           });
-          if (currentResource[0]) {
-            db.query(`SELECT *, username FROM resource_comments JOIN users ON user_id = users.id WHERE resource_id = $1 ORDER BY resource_comments.updated_at ASC;`, 
+          if (currentResource[0].title) {
+            db.query(`SELECT *, username FROM resource_comments JOIN users ON user_id = users.id WHERE resource_id = $1 LIMIT 3;`, 
             [currentResource[0].resource_id])
             .then((data) => {
-              const allComments = data.rows;
-              const templateVars = { 
-                resource: currentResource[0],
-                allComments: allComments,
-                loggedInUser: req.session.userId
-            };
-            console.log(currentResource[0]);
-            res.render("resource", templateVars);
-          });
+              const firstThreeComments = data.rows;
+              db.query(`SELECT *, username FROM resource_comments JOIN users ON user_id = users.id WHERE resource_id = $1 ORDER BY resource_comments.updated_at ASC;`, 
+              [currentResource[0].resource_id])
+              .then((data) => {
+                const allComments = data.rows;
+                const templateVars = { 
+                  resource: currentResource[0],
+                  firstThreeComments: firstThreeComments,
+                  allComments: allComments,
+                  loggedInUser: req.session.userId
+                };
+                res.render("resource", templateVars);
+              });
+            });
             
           } else {
           // error: no resource with id
