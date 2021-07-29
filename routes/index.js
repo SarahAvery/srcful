3bc0572572dceab0506f82a0b77fbdb24aa9d7ca
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-29 15:27:04 +0000

    Fix bug so when logged in, home page displays the correct nav bar

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,14 @@ const fetch = require("node-fetch");
 
 module.exports = () => {
   router.get("/", (req, res) => {
-    if (req.session.userId) {
-      res.redirect("/resources");
-    }
     fetch(process.env.API_URL + "/resources")
       .then((data) => data.json())
       .then((json) => {
         if (json.resources) {
-<<<<<<< HEAD
-          const templateVars = { resources: json.resources, pageNum:1 };
-=======
           const templateVars = { resources: json.resources, pageNum: 1 };
-
->>>>>>> main
+          if(req.session.userId) {
+            templateVars.user = req.session.userId;
+          }
           res.render("index", templateVars);
         } else {
           res.redirect("/");
