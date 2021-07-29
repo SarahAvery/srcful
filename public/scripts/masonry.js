87535ae6721d55cff3546e05f2fa2d1351bd595f
S Avery <sarahashavery@gmail.com>
2021-07-29 01:26:10 -0400

    Adding masonry

--- public/scripts/masonry.js
+++ public/scripts/masonry.js
@@ -1,5 +1,13 @@
 (function ($) {
   $(document).ready(function () {
+    const grid = $(".home-page .card-container").masonry({
+      itemSelector: ".card",
+      gutter: 10,
+      fitWidth: true,
+    });
 
-    // 
-  })(jQuery);
+    // recompute layout after 500ms to optimistically assume images will have finished loading
+    setTimeout(() => grid.masonry("layout"), 500);
+  });
+  // eslint-disable-next-line no-undef
+})(jQuery);
