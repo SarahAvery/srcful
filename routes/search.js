827d1813d050d036a6208ed30f132620b8ad4fe7
Kathy Tam <44310274+kathy-tam@users.noreply.github.com>
2021-07-29 12:31:11 -0500

    Feature/search (#120)
    * Add basic search template
    
    * Search feature setup
    
    * Add query to get search results
    
    * Display search results
    
    * Check for search match in resource description
    
    * Match search results case insensitive

---
+++ routes/search.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  router.post("/", (req, res) => {
+    const query = req.body.search.toLowerCase();
+    let results = [];
+    db.query('SELECT * FROM resources WHERE LOWER(title) LIKE $1 OR LOWER(description) LIKE $1', [`%${query}%`])
+    .then((data) => {
+      results = results.concat(data.rows);
+      const queryString =
+      `
+      SELECT resources.* FROM resources,
+      (SELECT resource_id FROM resource_categories,
+          (SELECT categories.id FROM categories
+          WHERE LOWER(title) LIKE $1) as t
+          WHERE resource_categories.category_id = t.id
+        ) as z
+      WHERE resources.id = z.resource_id;
+      `;
+      db.query(queryString, [`%${query}%`])
+      .then((data) => {
+        results = results.concat(data.rows);
+        res.render("search", { user: req.session.userId, results });
+      })
+    })
+    .catch((e) => res.send(e.stack));
+  });
+
+  return router;
+};
