4fa9d998b65010c20994afde76f2d3ef6be6e5a6
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-29 20:54:11 +0000

    Move /resources routes to /resource for edit and delete

--- routes/resources.js
+++ routes/resources.js
@@ -21,40 +21,5 @@ module.exports = (db) => {
       });
   });
 
-  // router.post("/new", (req, res) => {
-  //   const resource = req.body;
-  //   //All fields mandatory
-
-  //   let queryString = `
-  //   INSERT INTO resources (title, description, url, image_url, creator_id)
-  //   VALUES ($1, $2, $3, $4, $5)
-  //   RETURNING *;
-  //   `;
-  //   db.query(queryString, [
-  //     resource.title,
-  //     resource.description,
-  //     resource.url,
-  //     resource.imageURL,
-  //     req.session.userId,
-  //   ])
-  //     .then((data) => {
-  //       const newResource = data.rows[0];
-  //       resource.categories.forEach((category) => {
-  //         db.query(
-  //           `
-  //           INSERT INTO resource_categories(resource_id, category_id)
-  //             SELECT $1, id FROM categories WHERE title = $2;
-  //         `,
-  //           [newResource.id, category]
-  //         );
-  //       });
-  //     })
-  //     .then(() => res.redirect("/resources"))
-  //     .catch((e) => {
-  //       res.status(500);
-  //       res.send(e.stack);
-  //     });
-  // });
-
   return router;
 };
