83e11ad04dbbd3e40f36bff5e640fff61166f860
Kathy Tam <44310274+kathy-tam@users.noreply.github.com>
2021-07-29 22:15:32 -0500

    Get and update average rating for individual resource page (#138)

--- db/schema/06_resource_ratings.sql
+++ db/schema/06_resource_ratings.sql
@@ -4,14 +4,6 @@ CREATE TABLE resource_ratings (
   id SERIAL PRIMARY KEY NOT NULL,
   rating INTEGER NOT NULL DEFAULT 0,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  CONSTRAINT one_user_rating_per_resource UNIQUE (user_id, resource_id)
 );
-
-
-/* 
-pk  id serial
-fk  user_id: user.id
-fk  resource_id: resources.id
-
-rating: int
-*/
