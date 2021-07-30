0b6683a9de441d0f43887fae295786a30b83de74
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-30 02:04:47 +0000

    Get and update average rating for individual resource page

---
+++ routes/api/resource_ratings.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+
+module.exports = (db) => {
+
+  router.post("/", (req, res) => {
+    const rating = req.body.rating;
+    const resourceId = req.body.resourceId;
+
+    // console.log(rating);
+    // console.log(resourceId);
+
+    if (rating) {
+      db.query(`INSERT INTO resource_ratings (rating, user_id, resource_id)
+                VALUES ($1, $2, $3) ON CONFLICT (user_id, resource_id) DO UPDATE SET rating = $1;`,
+                [rating, req.session.userId, resourceId])
+      .then(() => {
+        db.query(`SELECT round(avg(resource_ratings.rating),1) AS avg_rating
+                  FROM resource_ratings
+                  JOIN resources ON resource_id = resources.id
+                  WHERE resource_id = $1
+                  GROUP BY resources.id;`,
+                  [resourceId])
+        .then((data) => res.json(data.rows[0]))
+      })
+      .catch(e => res.send(e.stack));
+    }
+
+  });
+
+  return router;
+
+};
